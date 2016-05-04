290c0cc1bf3677d4905c455fb0c0f918e0518de5
Magnus Leanderson <magnus_leanderson@hotmail.com>
2016-05-04 18:49:47 +0200

    Jag har kodat så man kna lägga till en ny användare "NewUser" samt en extra kontroll på password (så dem är samma) och gjort en kontroll om de är en elev eller admin/lärare

--- TestVerktyg_grp5/TestVerktyg_grp5/NewUser.xaml.cs
+++ TestVerktyg_grp5/TestVerktyg_grp5/NewUser.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TestVerktygLib;
 
 namespace TestVerktyg_grp5
 {
@@ -19,9 +20,50 @@ namespace TestVerktyg_grp5
     /// </summary>
     public partial class NewUser : Window
     {
+        Repository Repo = new Repository();
         public NewUser()
         {
             InitializeComponent();
+        }
+
+        private void btn_newUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(tbx_firstName.Text) ||
+                String.IsNullOrEmpty(tbx_lastName.Text) ||
+                String.IsNullOrEmpty(tbx_eMail.Text) ||
+                String.IsNullOrEmpty(tbx_password.Text)
+                )
+            {
+                MessageBox.Show("All fields must be filled");
+            }
+            else
+            {
+                if (tbx_password.Text == tbx_repeatPassword.Text)
+                {
+                    var newUser = new User()
+                    {
+                        FirstName = tbx_firstName.Text,
+                        LastName = tbx_lastName.Text,
+                        Email = tbx_eMail.Text,
+                        Password = tbx_password.Text,
+                        IsAdmin = cbx_adminTeacher.IsChecked.Value
+                    };
+
+
+                    Repo.AddUser(newUser);
+
+                    tbx_firstName.Text = "";
+                    tbx_lastName.Text = "";
+                    tbx_eMail.Text = "";
+                    tbx_password.Text = "";
+                    tbx_repeatPassword.Text = "";
+                    cbx_adminTeacher.IsChecked = false;
+                }
+                else
+                {
+                    MessageBox.Show("Not the same password!");
+                }
+            }
         }
     }
 }
