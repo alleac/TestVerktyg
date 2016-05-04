3f8494de9ac49dfcf9d86c5e3d391db314cd4692
Mattias Greger <greger.mattias@gmail.com>
2016-05-04 09:32:45 +0200

    Added Question class

---
+++ TestVerktyg_grp5/TestVerktygLib/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestVerktygLib
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AnswerOne { get; set; }
+        public string AnswerTwo { get; set; }
+        public string AnswerThree { get; set; }
+        public string AnswerFour { get; set; }
+        public int RightAnswer { get; set; }
+        public int HasAnsweredNumber { get; set; } = 0; // 0 is default
+
+    }
+}
